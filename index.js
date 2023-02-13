3b04657e8bfbb3f1d88ef6159c66fc83589df241
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-13 13:39:19 +0530

    fundamental of node js

---
+++ index.js
@@ -0,0 +1,71 @@
+// Fundamentals of Node js
+
+// const index = require("./node");
+// const index = require("OS");
+// console.log(index);
+
+// filter function
+/*
+// const ages = [32, 24, 36, 23, 16];
+// function checkAdult(age) {
+//   return age >= 18;
+// }
+// const result = ages.filter(checkAdult);
+// console.log(result);
+*/
+
+// const arr = [2, 4, 7, 1, 3, 8, 3];
+// let result = arr.filter((item) => {
+//   // return item === 3;
+//   return item > 3;
+//   // console.log(item);
+// });
+// console.log(result);
+
+// #######################//
+// Core modules
+
+/*1.Global module:we can't import
+2.Non-global module:we can import 
+*/
+// How to create a file in node js(modules)
+// const fs = require("fs");
+// fs.writeFileSync("hello.text", "hello world");
+// const fs = require("fs").writeFileSync;
+// fs("abc.text", "hey dude");
+
+// directory name
+// console.log(__dirname);
+// console.log(__filename);
+
+// #################################//
+// Make Basic Server
+/*
+const http = require("http");
+
+http
+  .createServer((req, resp) => {
+    resp.write("Hello this is vijay");
+    resp.end();
+  })
+  .listen(4600);
+*/
+/*
+const http = require("http");
+http
+  .createServer((req, resp) => {
+    resp.write("hello dear i am vijay");
+    resp.end();
+  })
+  .listen(2300);
+
+// *2nd way to create server
+const http = require("http");
+function dataControl(req, resp) {
+  resp.write("hy dear are you free then call me i am still waiting");
+  resp.end();
+}
+http.createServer(dataControl).listen(2400);
+
+// package.json//
+
---
+++ node.js
@@ -0,0 +1,32 @@
+/*
+//##########NODE js notes#########// 
+=> What is nod js:- 
+1.Node is not a language
+2.This is a sever environment
+4. Node js connect with database
+5. code and syntax is very similar to javascript but not exactly the same
+6.Node js is free open source
+Node js use Chrome's V8 engine to execute code.
+
+=>Why do we use node
+1.Node js mostly used for  create Api
+2.so we can connect the same database with web ,app,mobileApp,
+3.Node is easy to understand who know javascript.
+4.Node js is super-fast for APIs
+5. Node js run on the server side
+6.javaScript is run on the browser
+
+=> History 
+1. first release:may27,2009;
+2.current version:16.4
+written in:C,C++,javaScript
+
+
+=>Core Module in Node Js
+1.what are the core module
+ans:-
+*/
+module.exports = {
+  x: 30,
+  y: 40,
+};
