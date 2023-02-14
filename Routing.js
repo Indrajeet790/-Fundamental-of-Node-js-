f64cbc6cb9d03d1724ee48a0074356f956e866ff
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-14 20:21:33 +0530

    Routing.js

--- Routing.js
+++ Routing.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
+app.get("", (request, response) => {
+  console.log("data sent by browser =>>", request.query.name);
+  response.send("welcome ==>" + request.query.name);
+});
+// app.get("", (request, response) => {
+//   console.log("data sent by browser =>>", request.query.name);
+//   response.send("welcome this is home page");
+// });
+app.get("/about", (request, response) => {
+  response.send(" this is About pages");
+});
+app.get("/help", (request, response) => {
+  response.send("hello this is abcd pages");
+});
 
+app.listen(3000);
