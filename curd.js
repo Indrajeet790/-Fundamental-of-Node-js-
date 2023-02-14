522491763f70cdef7c48a6fdbfeb262ba326bf68
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-14 12:41:11 +0530

    promises problem

--- curd.js
+++ curd.js
@@ -66,3 +66,16 @@ fs.unlinkSync(`${dirPath}/studentName.txt`);
 // console.log(a + b);
 
 // ##############################################//
+// promises
+let a = 10;
+let b = 0;
+
+let waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(30);
+  }, 2000);
+});
+waitingData.then((data) => {
+  b = data;
+  console.log(a + b);
+});
