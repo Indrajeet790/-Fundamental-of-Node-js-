0d3fb52f627659ef57660ed45d9a4e142b78c329
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-16 15:38:59 +0530

    read data from MongoDb

---
+++ RDmongodb.js
@@ -0,0 +1,33 @@
+//  Read Data from MongoDb
+// Read Data from MongoDB
+// Make file for Db connection
+const { MongoClient } = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const database = "e-comm";
+const client = new MongoClient(url);
+
+async function dbConnect() {
+  let result = await client.connect();
+  db = result.db(database);
+  return db.collection("product");
+}
+// Handle promise
+
+// console.log(dbConnect());
+// handle promises
+// dbConnect().then((resp) => {
+//   resp
+//     .find()
+//     .toArray()
+//     .then((data) => {
+//       console.log(data);
+//     });
+// });
+
+//  another way to handle promises
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.log(data);
+};
+main();
